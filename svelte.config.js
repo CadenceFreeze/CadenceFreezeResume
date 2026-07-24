48b87b97d50b1e87c7e25b4299fb724fff8d7a42
Cadence <cfreeze@sram.com>
2026-07-24 13:56:01 -0600

    Configure GitHub Pages base path

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,12 @@ export default {
       assets: "build",
       fallback: "404.html",
     }),
+
+    paths: {
+      base: process.env.NODE_ENV === "production"
+        ? "/CadenceFreezeResume"
+        : "",
+    },
   },
 
   compilerOptions: {
