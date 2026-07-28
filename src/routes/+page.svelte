3ebd8d7287d3fbb4560f007ab9ee9c6540a5e019
Cadence <cfreeze@sram.com>
2026-07-28 13:47:39 -0600

    feature: add featured projects section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import FeaturedProjects from "$lib/components/sections/FeaturedProjects.svelte";
   import Hero from "$lib/components/sections/Hero.svelte";
 </script>
 
 <Hero />
+
+<FeaturedProjects />
